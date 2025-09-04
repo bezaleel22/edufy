0fbcad5d8d51706dce01acae18779c69aad5cd12
Onojeta Brown <onosbrown.saved@gmail.com>
2025-09-04 11:54:58 +0100

    Remove commented Google Maps API script from ScriptLoader

--- edufy-web/src/lib/components/ScriptLoader.svelte
+++ edufy-web/src/lib/components/ScriptLoader.svelte
@@ -9,9 +9,6 @@
   <script src="./js/jquery.datetimepicker.full.min.js"></script>
   <script src="./js/circle-progress.js"></script>
   <script src="./js/gmaps.js"></script>
-  <!-- <script
-    src="https://maps.google.com/maps/api/js?key=AIzaSyBJtPMZ_LWZKuHTLq5o08KSncQufIhPU3o"
-  ></script> -->
 
   <script src="./js/jquery.themepunch.tools.min.js"></script>
   <script src="./js/jquery.themepunch.revolution.min.js"></script>
